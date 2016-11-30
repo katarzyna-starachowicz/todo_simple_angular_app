e1b36d49035cf4588a71207f42ffa78c926d44ec
katarzyna-starachowicz <kasia@x8.io>
2016-11-30 18:33:13 +0100

    Implement adding todo functionality

--- todo-controller.js
+++ todo-controller.js
@@ -3,4 +3,9 @@ function TodoController($scope) {
     {text: 'Learn AngularJS', done: false},
     {text: 'Build new Angular app', done: false}
   ];
+
+  $scope.addTodo = function () {
+    $scope.todos.push({text:$scope.formTodoText, done: false});
+    $scope.formTodoText = '';
+  };
 }
