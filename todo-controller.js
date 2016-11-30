5a10ab181cb4af2a5b86c9130ce0e8c8878bf880
katarzyna-starachowicz <kasia@x8.io>
2016-11-30 23:22:22 +0100

    Implement clearing completed tasks

--- todo-controller.js
+++ todo-controller.js
@@ -8,6 +8,12 @@ function TodoController($scope) {
     return $scope.todos.length;
   };
 
+  $scope.clearCompleted = function () {
+      $scope.todos = _.filter($scope.todos, function(todo){
+          return !todo.done;
+      });
+  };
+
   $scope.addTodo = function () {
     $scope.todos.push({text:$scope.formTodoText, done: false});
     $scope.formTodoText = '';
